b1ff41fe7bf4f02c2085900ebfe9b6fc276b445a
Christopher Schultz <chris@christopherschultz.net>
2026-01-16 12:54:56 -0500

    Include the main JAR file first, if present
    This odd syntax is necessary to support POSIX shells that don't evaluate globs in assignments.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 MAIN_CLASS="${MAIN_CLASS:-com.koibots.scout.hub.Main}"
-java -classpath 'target/classes':'target/app/*':'target/dependency/*':src/main/resources/ "$MAIN_CLASS" "$@"
+MAIN_JAR=$( echo target/qr-scout-*.jar )
+java -classpath "$MAIN_JAR":'target/classes':'target/app/*':'target/dependency/*':src/main/resources/ "$MAIN_CLASS" "$@"
