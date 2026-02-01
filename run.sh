47259511fa57951c2f6e0128b4172f2303d9a3a2
Christopher Schultz <chris@christopherschultz.net>
2026-02-01 13:45:03 -0500

    Add optional JAVA_OPTS

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 MAIN_CLASS="${MAIN_CLASS:-com.koibots.scout.hub.Main}"
 MAIN_JAR=$( echo target/qr-scout-*.jar )
-java -classpath "$MAIN_JAR":'target/classes':'target/app/*':'target/dependency/*':src/main/resources/ "$MAIN_CLASS" "$@"
+java -classpath "$MAIN_JAR":'target/classes':'target/app/*':'target/dependency/*':src/main/resources/ $JAVA_OPTS "$MAIN_CLASS" "$@"
