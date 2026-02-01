b1ae3e309a2c28ebc77ed44e50b108e68497b04f
Christopher Schultz <chris@christopherschultz.net>
2026-01-31 19:44:37 -0500

    Improve UI proportions

--- src/main/java/com/koibots/scout/hub/SectionEditorDialog.java
+++ src/main/java/com/koibots/scout/hub/SectionEditorDialog.java
@@ -1,7 +1,9 @@
 package com.koibots.scout.hub;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Window;
 
 import javax.swing.*;
@@ -25,8 +27,19 @@ public class SectionEditorDialog
         nameField.setText(getUserObject().getName());
 
         JPanel center = new JPanel(new GridBagLayout());
-        center.add(new JLabel("Name:"));
-        center.add(nameField);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(4, 4, 4, 4);
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.gridx = 0;
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        center.add(new JLabel("Name:"), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        center.add(nameField, gbc);
 
         add(center, BorderLayout.CENTER);
         add(createButtonPanel(new JButton("Save")), BorderLayout.SOUTH);
